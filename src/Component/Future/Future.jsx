a51a1932976c1cf3d9af518a2eca649adfa4b62a
andreyz11288 <andreyz11288@gmail.com>
2021-07-02 14:20:09 +0300

    4

--- src/Component/Future/Future.jsx
+++ src/Component/Future/Future.jsx
@@ -6,11 +6,11 @@ function Future() {
     return (
         <section className={s.futureSection}>
             <div className={s.futureContainer}>
+                <img  className={s.img} src={img1} alt='img1' />
                 <div className={s.futureContainerText}>
                     <p>
                         The Exome+ assay provides the flexibility needed to build a comprehensive panel that can be expanded over time. With ~20,000 genes sequenced in each sample, every gene becomes a candidate for inclusion. And, while we have ensured the highest call rates across those genes that are most likely to belong to a panel, the Exome+ assay delivers high call rates for all genes beyond this core set.</p>
                 </div>
-                <img src={img1} alt='img1' />
             </div>
         </section>
     );
